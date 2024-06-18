c36d550170ad847486ae7422c2acadcc4fec8049
WaweruMauree <maureen.waweru@strathmore.edu>
2024-06-18 13:58:16 +0300

    updated sql code

--- sql/db.sql
+++ sql/db.sql
@@ -1,13 +1,16 @@
-USE 'website1';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS 'regions';
-CREATE TABLE IF NOT EXISTS 'regions'(
-'County' varchar(50) NOT NULL,
-'Reach' bigint(20) NOT NULL,
-'Supervisor' varchar(50) NOT NULL,
-'Email' varchar(50) NOT NULL,
-'Phone Number' tinyint(10) NOT NULL
-PRIMARY KEY ('County'),
-UNIQUE KEY ('Supervisor')
 
+
+DROP TABLE IF EXISTS `regions`;
+CREATE TABLE IF NOT EXISTS `regions`(
+`County` varchar(50) NOT NULL,
+`Reach` bigint(20) NOT NULL,
+`Supervisor` varchar(50) NOT NULL,
+`Email` varchar(50) NOT NULL,
+`Phone` int(10) NOT NULL,
+PRIMARY KEY (`County`),
+UNIQUE KEY (`Supervisor`)
 );
