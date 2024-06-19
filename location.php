bc47e7a80ee3d71257fa65ef543d8d93fb55e943
WaweruMauree <maureen.waweru@strathmore.edu>
2024-06-19 20:39:21 +0300

    php update

--- location.php
+++ location.php
@@ -1,4 +1,3 @@
-<html>
 <?php require_once ("templates/nav.php");?>
 <head>
 <meta charset="UTF-8">
@@ -10,26 +9,7 @@
 </head>
 
 <body>
-    <nav>
-        <div class="logo">WardrobeGalore</div>
-        <ul class="menu">
-            <li><a href="./index.html">Home</a></li>
-            <li>
-                <a href="./profile.html">Just For You</a>
-                <ul class="dropdown">
-                    <li><a href="#">Our Offers</a></li>
-                    <li><a href="#">Collections</a></li>
-                    <li><a href="#">Season Favorites</a></li>
-                </ul>
-            </li>
-            <li><a href="./aboutus.html">AboutUs</a></li>
-            <li><a href="./initiative.html">Initiative</a></li>
-            <li><a href="./interact.html">Our Socials</a></li>
-            <li><a href="./location.html">Locate Us</a></li>
-            <li><a href="./signin.html">Sign In</a></li>
-        </ul>
-        
-    </nav>
+   
     <br><br><br><br>
 
     <div class="container">
@@ -71,6 +51,6 @@
     </div>
 
 </body>
-</html>
+
 
 
