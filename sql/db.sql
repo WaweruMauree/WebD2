f4825fdd24f4302e45b8a6c8c52f788bdffa686f
WaweruMauree <maureen.waweru@strathmore.edu>
2024-07-08 10:36:50 +0300

    sql update

--- sql/db.sql
+++ sql/db.sql
@@ -10,22 +10,14 @@ SET time_zone = "+00:00";
 
 
 
-DROP TABLE IF EXISTS `regions`;
-CREATE TABLE IF NOT EXISTS `regions`(
-`County` varchar(50) NOT NULL,
-`Reach` bigint(20) NOT NULL,
-`Supervisor` varchar(50) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users`(
+`Name` varchar(50) NOT NULL,
+`Phone_No.` tinyint(10) NOT NULL,
 `Email` varchar(50) NOT NULL,
-`Phone` int(10) NOT NULL,
-PRIMARY KEY (`County`),
-UNIQUE KEY (`Supervisor`)
+`Message` varchar(1000) NOT NULL,
+PRIMARY KEY (`Name`),
+UNIQUE KEY (`Phone_No.`)
 );
 
-INSERT INTO `regions`(`County`,`Reach`,`Supervisor`,`Email`,`Phone`)
-VALUES('Turkana','10000','Mr.Nick Macharia','nickmacharia@gmail.com','0723467895'),
-('Kisumu','5000','Dr.Angel Kibet','angelkibet@gmail.com','0734567234'),
-('Narok','13050','Rev.William Kimani','wkimani@gmail.com','0712546798'),
-('Kakamega','14000','Mrs.Julia Kathingu','juliakat@gmail.com','0789567356'),
-('Kiambu','1200','Mr.Henry Kiarie','hkiarie@gmail.com','0798563423');
-
-SELECT * FROM `regions`;
+SELECT * FROM `users`;
