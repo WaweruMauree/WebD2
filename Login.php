6c0d70fcf79cbea8d68134397373894b25df6819
WaweruMauree <maureen.waweru@strathmore.edu>
2024-07-08 10:39:04 +0300

    user login page

---
+++ Login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/nav.css">
+    <link rel="stylesheet" href="">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <nav>
+        <div class="logo">WardrobeGalore</div>
+        <ul class="menu">
+            <li><a href="./index.html">Home</a></li>
+            <li>
+                <a href="./profile.html">Just For You</a>
+                <ul class="dropdown">
+                    <li><a href="#">Our Offers</a></li>
+                    <li><a href="#">Collections</a></li>
+                    <li><a href="#">Season Favorites</a></li>
+                </ul>
+            </li>
+            <li><a href="./aboutus.html">AboutUs</a></li>
+            <li><a href="./initiative.html">Initiative</a></li>
+            <li><a href="./interact.html">Our Socials</a></li>
+            <li><a href="./location.html">Locate Us</a></li>
+            <li><a href="./signin.html">Sign In</a></li>
+        </ul>
+    </nav>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Database connection details
+            $servername = "localhost"; 
+            $username = "root"; 
+            $password = "";  
+            $database = "website1"; 
+
+            $conn = mysqli_connect($servername, $username, $password, $database);
+
+            // Check connection
+            if($conn === false){
+                die("ERROR: Could not connect. " . mysqli_connect_error());
+            }
+
+            // Taking input values from the form
+            $user_name = $_POST['username'];
+            $password = $_POST['password'];
+
+            // Checking if the user exists in the database
+            $sql = "SELECT * FROM users WHERE username = '$user_name' AND password = '$password'";
+            $result = mysqli_query($conn, $sql);
+
+            if(mysqli_num_rows($result) > 0){
+                echo "<h3>Welcome, $user_name!</h3>";
+            } else{
+                echo "<h3>Not a verified customer</h3>";
+            }
+
+            // Close connection
+            mysqli_close($conn);
+        }
+        ?>
+    </div>
+</body>
+</html>
