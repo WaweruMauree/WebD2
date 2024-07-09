9283ca9d690a92b3fe62bee3732c532d37c2c4cd
WaweruMauree <maureen.waweru@strathmore.edu>
2024-07-09 10:30:53 +0300

    location page commit

--- location.php
+++ location.php
@@ -17,7 +17,6 @@
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8182391463865!2d36.820606973111275!3d-1.2828851356185338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f110007200d07%3A0x317a661e12a0e0e6!2ssawa%20mall!5e0!3m2!1sen!2ske!4v1718341122410!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
     </div>
-
      <!--contact start-->
     <section class="contact" id="contact">
         <div class="main-contact">
