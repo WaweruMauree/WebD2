373b6c83a5bcbb381128fb1c6a7997ec4964bfd3
WaweruMauree <maureen.waweru@strathmore.edu>
2024-07-09 07:39:33 +0300

    view user update

--- View_user.php
+++ View_user.php
@@ -9,6 +9,7 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
+  
     <nav>
         <div class="logo">WardrobeGalore</div>
         <ul class="menu">
