c98cecaf8b54a860d6467c701c7ebf712baf00b8
WaweruMauree <maureen.waweru@strathmore.edu>
2024-07-09 10:28:28 +0300

    about page commit

--- aboutus.php
+++ aboutus.php
@@ -14,7 +14,6 @@
             <h2 class="title">Our Founder</h2>
         </div>
         <p> 
-         
             In the heart of Nairobi, Kenya, Amina's fascination with fashion knew no bounds. She saw clothing not just as garments but as a medium for expression, a way to bridge cultures and tell stories. With this vision in mind, she embarked on a journey to create something truly unique in the bustling fashion landscape of Kenya.
    
             Inspired by the vibrant diversity of Nairobi's streets, Amina founded Wardrobe Galore, a brand that aimed to redefine fashion by embracing global influences. Unlike traditional clothing companies that focused solely on African textiles, Amina wanted Wardrobe Galore to celebrate the beauty of fabrics from around the world, from the silk of China to the denim of America.
